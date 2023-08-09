7de5bf30c102aa4e2a65f044891bd2105c163846
Carolina Costa Miranda <carolinacosta@MacBook-Air-de-Carolina.local>
2023-08-09 14:17:23 -0300

    Update conftest.py

--- pythonProject/conftest.py
+++ pythonProject/conftest.py
@@ -2,7 +2,8 @@ import pytest
 
 from selenium import webdriver
 
-driver: webdriver.Remote
+# Indicando a URL do serviço remoto do Selenium
+REMOTE_URL = "http://localhost:4444/wd/hub"
 
 
 @pytest.fixture
@@ -11,7 +12,8 @@ def setup_teardown():
     global driver
     options = webdriver.ChromeOptions()
 
-    driver = webdriver.Chrome(options=options)
+    # Configurando o driver para usar o serviço remoto
+    driver = webdriver.Remote(command_executor=REMOTE_URL, desired_capabilities=options.to_capabilities())
 
     driver.implicitly_wait(10)
     driver.maximize_window()
